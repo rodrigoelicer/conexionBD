ffca7187f317587975a08e204602ccf98375d257
Umauro <felipe.vega.14@sansano.usm.cl>
2017-10-12 11:54:00 -0300

    minor changes

--- my-app2/server.js
+++ my-app2/server.js
@@ -5,12 +5,14 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 // Get our API routes
 const api = require('./server/routes/api');
 
 const app = express();
 
+app.use(morgan('dev'));
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
