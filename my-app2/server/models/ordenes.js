ecd7546ce2fc61cb7fb70347383005cbca9c6585
Rodrigo Elicer <rodrigo.elicer1@gmail.com>
2017-11-18 17:45:36 -0300

    agregando órdenes a bd

--- my-app2/server/models/ordenes.js
+++ my-app2/server/models/ordenes.js
@@ -2,20 +2,20 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('ordenes', {
     idordenes: {
       type: DataTypes.INTEGER(11),
+      autoIncrement: true,
       allowNull: false,
       primaryKey: true
     },
     descripcion: {
       type: DataTypes.STRING(45),
       allowNull: true
-  	},
-  	fecha :{
-		type: DataTypes.DATE,
-		allowNull: true
-	}
+    },
+    fecha: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    }
   }, {
     tableName: 'ordenes'
   });
 };
-
-//Notar que el modelo fue realizado en base a la base de datos
--- my-app2/server/routes/api.js
+++ my-app2/server/routes/api.js
@@ -6,30 +6,31 @@ const path = require('path')
 
 //Add Sequelize as ORM //
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('test', 'root', '1234',{//bd,user,pass
-	host: 'localhost',
-	dialect: 'mysql',
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	},
+const sequelize = new Sequelize('test', 'root', '1234', { //bd,user,pass
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
   operatorsAliases: false,
-	define: {
-		defaultScope:{
-			attributes:{
-				exclude: ['createdAt','updatedAt']
-			}
-		}
-	}
+  define: {
+    timestamps: false,
+    defaultScope: {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      }
+    }
+  }
 });
 // Recuerden que el JSON de conexión se puede dejar en un archivo aparte
 
 //Test de conexión
-sequelize.authenticate().then(() =>	{
-	console.log("Conexión establecida");
+sequelize.authenticate().then(() => {
+  console.log("Conexión establecida");
 }).catch(err => {
-	console.error("No te puedes conectar: ", err);
+  console.error("No te puedes conectar: ", err);
 });
 
 // Import User Models
@@ -48,6 +49,22 @@ router.get('/orden', (req, res) => {
   Orden.findAll().then(ordenes => {
     res.status(200).send(ordenes);
   })
+});
+
+router.post('/crearOrden', (req, res) => {
+  Orden.create({
+    idordenes: req.body.id,
+    descripcion: req.body.descripcion,
+  }).then(function(order) {
+    res.json({
+      status: 200,
+      body: {
+        result: "OK"
+      }
+    });
+  }).catch(function(err) {
+    console.log(err)
+  });
 });
 
 module.exports = router;
